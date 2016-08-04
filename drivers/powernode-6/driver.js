153757368b414f7e751bfbacb86a502f7973b37e
Emile Nijssen <emile@athom.nl>
2016-08-04 17:19:17 +0200

    renamed driver to powernode-6

--- drivers/powernode-6/driver.js
+++ drivers/powernode-6/driver.js
@@ -6,17 +6,19 @@ const ZwaveDriver	= require('homey-zwavedriver');
 // http://www.pepper1.net/zwavedb/device/280
 
 module.exports = new ZwaveDriver( path.basename(__dirname), {
-	debug: true,
+	//debug: true,
 	capabilities: {
 		'onoff': {
 			'command_class'				: 'COMMAND_CLASS_SWITCH_BINARY',
 			'command_get'				: 'SWITCH_BINARY_GET',
+			'command_get_cb'			: false,
 			'command_set'				: 'SWITCH_BINARY_SET',
 			'command_set_parser'		: function( value ){
 				return {
 					'Switch Value': value
 				}
 			},
+			'command_report'			: 'SWITCH_BINARY_REPORT',
 			'command_report_parser'		: function( report ){
 				return report['Value'] === 'on/enable';
 			},
@@ -25,6 +27,7 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 		'meter_power': {
 			'command_class'				: 'COMMAND_CLASS_METER',
 			'command_get'				: 'METER_GET',
+			'command_get_cb'			: false,
 			'command_get_parser'		: function(){
 				return {
 					'Properties1': {
