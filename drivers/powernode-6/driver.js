a5670c4fe6fd05fb02225421c890824c49c447d8
Robin Bolscher <robinbolscher@gmail.com>
2016-09-10 16:26:23 +0200

    Fix faulty merge

--- drivers/powernode-6/driver.js
+++ drivers/powernode-6/driver.js
@@ -6,8 +6,6 @@ const ZwaveDriver = require('homey-zwavedriver');
 // http://www.pepper1.net/zwavedb/device/280
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
-<<<<<<< HEAD
-	//debug: true,
 	capabilities: {
 		'onoff': {
 			'command_class': 'COMMAND_CLASS_SWITCH_BINARY',
@@ -15,27 +13,12 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			'command_get_cb': false,
 			'command_set': 'SWITCH_BINARY_SET',
 			'command_set_parser': function (value) {
-=======
-	debug: false,
-	capabilities: {
-		'onoff': {
-			'command_class'				: 'COMMAND_CLASS_SWITCH_BINARY',
-			'command_get'					: 'SWITCH_BINARY_GET',
-			'command_get_cb'			: false,
-			'command_set'					: 'SWITCH_BINARY_SET',
-			'command_set_parser'	: function(value) {
->>>>>>> 837ba3eb2af6b6e4e0b08312ac0a01d484442e24
 				return {
 					'Switch Value': value
 				}
 			},
-<<<<<<< HEAD
 			'command_report': 'SWITCH_BINARY_REPORT',
 			'command_report_parser': function (report) {
-=======
-			'command_report'				: 'SWITCH_BINARY_REPORT',
-			'command_report_parser'	: function(report) {
->>>>>>> 837ba3eb2af6b6e4e0b08312ac0a01d484442e24
 				return report['Value'] === 'on/enable';
 			},
 			'pollInterval': "poll_interval"
@@ -64,24 +47,16 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		},
 
 		'meter_power': {
-<<<<<<< HEAD
 			'command_class': 'COMMAND_CLASS_METER',
 			'command_get': 'METER_GET',
 			'command_get_cb': false,
 			'command_get_parser': function () {
-=======
-			'command_class'				: 'COMMAND_CLASS_METER',
-			'command_get'					: 'METER_GET',
-			'command_get_cb'			: false,
-			'command_get_parser'	: function() {
->>>>>>> 837ba3eb2af6b6e4e0b08312ac0a01d484442e24
 				return {
 					'Properties1': {
 						'Scale': 0
 					}
 				}
 			},
-<<<<<<< HEAD
 			'command_report': 'METER_REPORT',
 			'command_report_parser': function (report) {
 				if (report && report.hasOwnProperty('Properties2')
@@ -90,27 +65,17 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 
 					return report['Meter Value (Parsed)'];
 				} else return null;
-=======
-			'command_report'				: 'METER_REPORT',
-			'command_report_parser'	: function(report) {
-				return report['Meter Value (Parsed)'];
->>>>>>> 837ba3eb2af6b6e4e0b08312ac0a01d484442e24
 			},
 			'pollInterval': "poll_interval"
 		}
 	},
-<<<<<<< HEAD
-	settings: {}
-})
-=======
 	settings: {
 		"keep_alive_time": {
 			"index": 1,
 			"size": 1,
-			"parser": function(input) {
+			"parser": function (input) {
 				return new Buffer([parseInt(input)]);
 			}
 		}
 	}
 });
->>>>>>> 837ba3eb2af6b6e4e0b08312ac0a01d484442e24
